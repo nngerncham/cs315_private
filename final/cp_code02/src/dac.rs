445af8a4e6b9bfd3f225f656042f9daf08e5e75a
nawat <n.ngerncham@gmail.com>
2023-04-07 09:26:44 +0700

    div and con cp added

--- final/cp_code02/src/dac.rs
+++ final/cp_code02/src/dac.rs
@@ -1,10 +1,11 @@
 use crate::point::{dist, pt_default, Distance, Point};
 
-fn closest_pairs(points: &[Point]) -> (Point, Point, Distance) {
+fn closest_pairs(points: &[Point]) -> [Point; 2] {
     let mut sorted_points = points.to_vec();
     sorted_points.sort_by_key(|&(x, _)| x);
 
-    cp_helper(&mut sorted_points)
+    let (p1, p2, _) = cp_helper(&mut sorted_points);
+    [p1, p2]
 }
 
 fn cp_helper(points: &[Point]) -> (Point, Point, Distance) {
@@ -22,7 +23,7 @@ fn cp_helper(points: &[Point]) -> (Point, Point, Distance) {
     let (rp1, rp2, rd) = cp_helper(r);
 
     // step 2, 3
-    let (min_p1, min_p2, d) = if ld < rd {
+    let (mut min_p1, mut min_p2, mut d) = if ld < rd {
         (lp1, lp2, ld)
     } else {
         (rp1, rp2, rd)
@@ -35,12 +36,31 @@ fn cp_helper(points: &[Point]) -> (Point, Point, Distance) {
         .filter(|&(x, _)| ((x_mid - x).abs() as i64) < d)
         .cloned()
         .collect();
+
     if band.len() <= 1 {
         return (min_p1, min_p2, d);
+    } else if band.len() == 2 {
+        let (band_p1, band_p2, band_d) = (band[0], band[1], dist(&band[0], &band[1]));
+        if band_d < d {
+            // band min smaller
+            return (band_p1, band_p2, band_d);
+        } else {
+            // outside of band min smaller
+            return (min_p1, min_p2, d);
+        }
     }
     band.sort_by_key(|&(_, y)| y);
 
-    (pt_default(), pt_default(), 0i64)
+    for i in 1..band.len().min(7usize) {
+        let p1 = band[i - 1];
+        let p2 = band[i];
+
+        if dist(&p1, &p2) < d {
+            (min_p1, min_p2, d) = (p1, p2, dist(&p1, &p2));
+        }
+    }
+
+    (min_p1, min_p2, d)
 }
 
 mod test {
@@ -49,10 +69,10 @@ mod test {
         use std::collections::hash_set::HashSet;
         let points = vec![(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)];
         dbg!(super::closest_pairs(&points));
-        // assert_eq!(
-        //     HashSet::from([(2, 3), (3, 4)]),
-        //     HashSet::from(super::closest_pairs(&points))
-        // );
+        assert_eq!(
+            HashSet::from([(2, 3), (3, 4)]),
+            HashSet::from(super::closest_pairs(&points))
+        );
     }
 
     #[test]
@@ -160,9 +180,9 @@ mod test {
             (9, -8),
             (-75, -85),
         ];
-        // assert_eq!(
-        //     HashSet::from([(26, -21), (25, -19)]),
-        //     HashSet::from(super::closest_pairs(&points))
-        // );
+        assert_eq!(
+            HashSet::from([(26, -21), (25, -19)]),
+            HashSet::from(super::closest_pairs(&points))
+        );
     }
 }
